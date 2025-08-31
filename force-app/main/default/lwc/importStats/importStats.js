caadde50e4bbae68f0cf6e60d5606064a057a70e
Gustavo Fonseca <gu.fonsecaa@gmail.com>
2025-08-31 02:51:59 -0300

    wip: create queuable to start import process

--- force-app/main/default/lwc/importStats/importStats.js
+++ force-app/main/default/lwc/importStats/importStats.js
@@ -1,6 +1,16 @@
 import { api, LightningElement, track } from 'lwc';
+import {
+    subscribe,
+    unsubscribe,
+    onError,
+    setDebugFlag,
+    isEmpEnabled,
+} from 'lightning/empApi';
 
 import getImportRecordData from '@salesforce/apex/DataImporterController.getImportRecordData';
+import startImport from '@salesforce/apex/DataImporterController.startImport';
+
+const IMPORT_PROGRESS_CHANNEL = '/event/ImportProgress__e';
 
 export default class ImportStats extends LightningElement {
   @api fileInfo;
@@ -9,6 +19,8 @@ export default class ImportStats extends LightningElement {
 
   @track isLoading = true;
   @track importData;
+
+  @track subscription = {};
 
   get isProcessingStatus() {
     return this.importData?.status === 'Processing';
@@ -22,11 +34,23 @@ export default class ImportStats extends LightningElement {
     this.fetchImportData();
   }
 
+  disconnectedCallback() {
+    if (this.subscription) {
+      unsubscribe(this.subscription, () => {
+          console.log('Unsubscribed from platform event');
+          this.subscription = null;
+      });
+    }
+  }
+
   async fetchImportData() {
     try {
       const response = await getImportRecordData({ importRecordId: this.importRecordId });
       const data = JSON.parse(response);
       this.importData = data;
+
+      this.handleSubscribe();
+
     } catch (error) {
       console.log('Error to fetch import record.');
       console.error(error);
@@ -36,6 +60,33 @@ export default class ImportStats extends LightningElement {
   }
 
   async handleStartImport() {
+    try {
+      await startImport({ importRecordId: this.importRecordId });
+    } catch (error) {
+      console.log('Error to start import.');
+      console.error(error);
+    }
+  }
 
+  handleSubscribe() {
+    const callback = (response) => {
+      const { payload } = response.data;
+      const importData = this.importData;
+      if (this.importData.id === payload.ImportId__c) {
+        if (payload.Status__c) {
+          importData.status = payload.Status__c;
+        }
+        this.importData = importData;
+      }
+    };
+
+    subscribe(IMPORT_PROGRESS_CHANNEL, -1, callback).then(response => {
+      console.log('Subscription to channel ' + IMPORT_PROGRESS_CHANNEL + ' is successful');
+      this.subscription = response;
+    });
+
+    onError(error => {
+      console.error('Error subscribing to platform event:', error);
+    });
   }
 }
